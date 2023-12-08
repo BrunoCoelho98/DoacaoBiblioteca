77777d904e91c9583f7d4b11cc572f6bec2dbc2e
Bruno <zexxed@fedora>
2023-12-07 23:30:41 -0300

    Adicionado alguns comentários

--- src/main/java/com/mycompany/doacaobiblioteca/Livro.java
+++ src/main/java/com/mycompany/doacaobiblioteca/Livro.java
@@ -4,27 +4,41 @@
  */
 package com.mycompany.doacaobiblioteca;
 
+import java.io.Serializable;
+
 /**
  *
- * @author bruno
+ * @author Bruno Coelho
  */
-public class Livro {
-    private String nome;
+public class Livro implements Serializable {
+    
+    private String titulo;
     private String autor;
     private int isbn;
     private String categoria;
     private int anoPublicacao;
+    private String condicao;
+    private Expedicao expedicao;
 
-    public Livro(String nome, String autor, int isbn, String categoria, int anoPublicacao) {
-        this.nome = nome;
+    public Livro(String titulo, String autor, int isbn, String categoria, int anoPublicacao, String condicao) {
+        this.titulo = titulo;
         this.autor = autor;
         this.isbn = isbn;
         this.categoria = categoria;
         this.anoPublicacao = anoPublicacao;
+        this.condicao = condicao;        
     }
 
-    public String getNome() {
-        return nome;
+    public String getLocal() {
+        return this.expedicao.getLocal();
+    }
+    
+    public String getDataExpedicao(){
+        return this.expedicao.getData();
+    }
+   
+    public String getTitulo() {
+        return titulo;
     }
 
     public String getAutor() {
@@ -42,10 +56,49 @@ public class Livro {
     public int getAnoPublicacao() {
         return anoPublicacao;
     }
+
+    public String getCondicao() {
+        return condicao;
+    }
+
+    public void setCondicao(String condicao) {
+        this.condicao = condicao;
+    }
+    
+    public void expedir(String data, String local){
+        this.expedicao = new Expedicao(this, data, local);
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public void setIsbn(int isbn) {
+        this.isbn = isbn;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public void setAnoPublicacao(int anoPublicacao) {
+        this.anoPublicacao = anoPublicacao;
+    }
+
+    public void setExpedicao(Expedicao expedicao) {
+        this.expedicao = expedicao;
+    }
     
     
-    
-    
+
+    @Override
+    public String toString() {
+        return "Livro{" + "titulo=" + titulo + ", autor=" + autor + ", isbn=" + isbn + ", categoria=" + categoria + ", anoPublicacao=" + anoPublicacao + ", condicao=" + condicao + ", expedicao=" + expedicao;
+    }
     
     
     
